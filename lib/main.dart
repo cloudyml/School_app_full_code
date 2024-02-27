8129403f5e142aa275c492bd9662e7c7f5360ed5
Arpan-mukherje <mukherjee.arpan@hotmail.com>
2024-02-27 17:19:22 +0530

    Logout Problem solved

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,10 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+import 'package:school_management_system/Screens/Splash/splashScreen.dart';
 import 'package:school_management_system/Services/Provider/attendance_provider.dart';
 import 'package:school_management_system/Services/firebase_api_services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'Screens/Splash/splashScreen.dart';
 import 'constants/constants.dart';
 
 void main() async {
@@ -22,7 +22,6 @@ void main() async {
           messagingSenderId: "167650873505",
           projectId: "school-app-94046"),
     );
-    
   } else {
     await Firebase.initializeApp();
     await FirebaseApi().initNotification();
